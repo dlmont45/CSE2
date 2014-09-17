ab8d4c2747eea79bef02399bf3357b27a2e0de53
dlmont45 <dsl218@lehigh.edu>
2014-09-17 02:58:22 +0000

    updated code

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -36,10 +36,32 @@
         myScanner = new Scanner ( System.in ); //Constructing an instance of the scanner class
 
 //  Input Variables
-        System.out.print("Enter number:"); //Prompt user to enter number
+        System.out.print("Enter a double and I will display the first four digits to the right of the decimal:"); //Prompt user to enter number
+        double number = myScanner.nextDouble();
+        
+        double IntNumber; //Integer of double 
+        double n1; //10th place
+        double n2; //100th place
+        double n3; //1000th place
+        double n4; //10,000th place
+        double decimals; //digits right of the decimal
+        double FinalDecimals; //Final Decimals
         
 //  Begin code for FourDigits
+        IntNumber = (int)(number); //Trunkated number of double
+        decimals = (number - IntNumber)*1000; //Calculating the decimals
+        n1 = (int)(decimals*10); //Calculating n1
+        n2 = ((int)(decimals*100)-(n1*10)); //Calculating n2
+        n3 = ((int)(decimals*1000)-(n1*100)-(n2*10)); //Calculating n3
+        n4 = ((int)(decimals*10000)-(n1*1000)-(n2*100)-(n3*10)); //Calculating n4
+        FinalDecimals = (int)(n1*1000)+(n2*100)+(n3*10)+(n4); //Calculating final decimal
 
+// Print Output
+        System.out.println("The four digits are: "+FinalDecimals); //Print final four digits
+        
+        
+        
+        //testtest/test
 
 
         } //End of Method
